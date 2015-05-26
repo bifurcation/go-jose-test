db5c85d087983e07075a0005d85c53f38a9d70a8
Richard Barnes <rlb@ipv.sx>
2015-05-26 11:36:45 -0400

    Add actual verification, instead of just parsing

--- jose-validate.go
+++ jose-validate.go
@@ -15,21 +15,28 @@ func main() {
 		return
 	}
 
-	jws, err := jose.ParseSigned(string(bytes))
-
+	// Phase 1: Parsing
+	parsedJws, err := jose.ParseSigned(string(bytes))
 	if err != nil {
-		fmt.Println("validation: FAIL")
+		fmt.Println("parsing: FAIL")
 		fmt.Println(err)
 		return
 	} else {
-		fmt.Println("validation: OK")
+		fmt.Println("parsing: OK")
 	}
 
-	compact, err := jws.CompactSerialize()
-	if err != nil {
-		fmt.Println(err)
+	// Phase 2: Verification
+	if len(parsedJws.Signatures) != 1 {
+		fmt.Println("Must have exactly one signature, instead of ", len(parsedJws.Signatures))
 		return
 	}
-
-	fmt.Println(compact)
+	key := parsedJws.Signatures[0].Header.JsonWebKey
+	_, err = parsedJws.Verify(key)
+	if err != nil {
+		fmt.Println("verification: FAIL")
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println("verification: OK")
+	}
 }
